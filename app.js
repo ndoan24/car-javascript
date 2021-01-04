043faeba0024bb38ede06725191f1ab251c569fb
Ngoc Doan <ngocdoan241@gmail.com>
2021-01-04 16:11:48 -0700

    second

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 
+var tommyBird = new Object(); {
+    tommyBird.age=38;
+    tommyBird.color= "white";
+    tommyBird.likes= ["food","sun"];
+    tommyBird.birthday= { "month": 7, "day": 17, year: 1983 }
+};
+
+function describeBird(bird) {
+    console.log("This bird is " + bird.age + " years old with " + bird.color + " feathers.");
+}
+
+describeBird(tommyBird);
+
 
 
 
 
 //The constuctor function 
+
+
 //A method of the object
+
+
 //Instantiating a new object

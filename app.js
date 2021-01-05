ee51aea52c639991f110ee5657067b38efcf183e
Ngoc Doan <ngocdoan241@gmail.com>
2021-01-04 17:44:19 -0700

    lemon

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 
-var tommyBird = new Object(); {
-    tommyBird.age=38;
-    tommyBird.color= "white";
-    tommyBird.likes= ["food","sun"];
-    tommyBird.birthday= { "month": 7, "day": 17, year: 1983 }
-};
+// var tommyBird = new Object(); {
+//     tommyBird.age=38;
+//     tommyBird.color= "white";
+//     tommyBird.likes= ["food","sun"];
+//     tommyBird.birthday= { "month": 7, "day": 17, year: 1983 }
+// };
 
-function describeBird(bird) {
-    console.log("This bird is " + bird.age + " years old with " + bird.color + " feathers.");
-}
+// function describeBird(bird) {
+//     console.log("This bird is " + bird.age + " years old with " + bird.color + " feathers.");
+// }
 
-describeBird(tommyBird);
+// describeBird(tommyBird);
+
+
+
+
+
+// //The constuctor function 
+
+// function Movie (title, year, genre, cast) {
+//     this.title= title;
+//     this.year=year;
+//     this.genre= genre;
+//     this.cast= [cast];
+// }
+
+
+// //A method of the object
+
+// Movie.prototype.read=function() {
+//     document.write("Title" + this.title + "Year " + this.year + "Genre " + this.genre + "Actors: " + this.cast)
+// }
+
+
+
+// //Instantiating a new object
+
+// var movie1= new Movie ("The Color Purple", 1982, "Drama", ["whoopi", "oprah"]);
+// movie1.read();
 
 
 
@@ -18,8 +45,23 @@ describeBird(tommyBird);
 
 //The constuctor function 
 
+function Car (title, year, make, model) {
+    this.title= title;
+    this.year=year;
+    this.make= make;
+    this.model= model;
+}
+
 
 //A method of the object
 
+Car.prototype.read=function() {
+    document.write("Title" + this.title + "Year " + this.year + "Make " + this.make + "Model " + this.model)
+}
+
+
 
 //Instantiating a new object
+
+var car1= new Car ("Tesla", 2018, "Tesla", ModelS);
+car1.read();
